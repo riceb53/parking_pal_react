d3f33e9e43fa65cebc21a94e030445192980f6af
brian <riceb53@gmail.com>
2023-05-18 16:54:18 -0700

    show citations on map

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,20 +1,25 @@
 import axios from "axios"
-import { useEffect } from "react"
-import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import { useEffect, useState } from "react"
+import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
 import { useMemo } from "react";
 import "./Content.css";
 
 const apiKey = import.meta.env.VITE_GOOGLE_API_KEY
 
+
+
 export function Content() {
+  const [citations, setCitations] = useState([])
+  
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: apiKey,
   });
-  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+  const center = useMemo(() => ({ lat: 48.8566, lng: 2.3522 }), []);
 
   
   const getCitations = () => {
     axios.get("http://localhost:8000/").then(response => {
+      setCitations(response.data)
       console.log(response.data)
     })
   }
@@ -31,12 +36,25 @@ export function Content() {
           mapContainerClassName="map-container"
           center={center}
           zoom={10}
-        />
+        >
+          {citations.map(citation => (
+            <MarkerF
+              position={{ lat: citation.latitude, lng: citation.longitude
+              }}
+              icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"}
+            />
+
+          ))}
+        </GoogleMap>
       )}
     </div>
 
     </div>
   )
 }
+
+
+
+
 
 
