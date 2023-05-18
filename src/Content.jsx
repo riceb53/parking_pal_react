21335f73dae73065fe4a725adae8ebe704172775
brian <riceb53@gmail.com>
2023-05-18 12:00:48 -0700

    add google maps, get citations from api. not rendering citations in map yet

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,42 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import { useMemo } from "react";
+import "./Content.css";
+
+const apiKey = import.meta.env.VITE_GOOGLE_API_KEY
+
 export function Content() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: apiKey,
+  });
+  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+
+  
+  const getCitations = () => {
+    axios.get("http://localhost:8000/").then(response => {
+      console.log(response.data)
+    })
+  }
+
+  useEffect(getCitations, [])
   return (
     <div>
       <h1>Welcome to React!</h1>
+      <div className="App">
+      {!isLoaded ? (
+        <h1>Loading...</h1>
+      ) : (
+        <GoogleMap
+          mapContainerClassName="map-container"
+          center={center}
+          zoom={10}
+        />
+      )}
+    </div>
+
     </div>
   )
 }
+
+
