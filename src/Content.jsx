4537f9908156dcb36a3167e03a61aee3d1487962
brian <riceb53@gmail.com>
2023-06-01 11:11:18 -0700

    show more info on marker click, handle new data from api

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,12 +1,10 @@
 import axios from "axios"
 import { useEffect, useState, useMemo } from "react"
-import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
+import { GoogleMap, MarkerF, useLoadScript, InfoWindowF } from "@react-google-maps/api";
 import "./Content.css";
 
 
-const apiKey = import.meta.env.VITE_GOOGLE_API_KEY
-
-
+const apiKey = import.meta.env.VITE_GOOGLE_API_KEY_2
 
 export function Content() {
   const [citations, setCitations] = useState([])
@@ -14,32 +12,60 @@ export function Content() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: apiKey,
   });
+
   // const center = useMemo(() => ({ lat: 37.7827488, lng: -122.4144937 }), []);
-  const [center, setCenter] = useState({})
+  const [center, setCenter] = useState({lat: 37.776497, lng: -122.441658})
+  const [address, setAddress] = useState("1500 Fulton St San Francisco, CA")  
+  const [selectedCitation, setSelectedCitation] = useState(null)
+  const [mostFrequentHour, setMostFrequentHour] = useState(0)
+  const [q1q3, setq1q3] = useState([0,1])
+  const [q1q3StrClean, setq1q3StrClean] = useState([0,1])
+  // const 
+
+  
 
   
   const getCitations = () => {
-    axios.get("http://localhost:8000/").then(response => {
+    axios.get(`http://localhost:8000/?q=${address}`).then(response => {
       setCitations(response.data.citations)
       setCenter({lat: response.data.closest_coordinates.latitude, lng: response.data.closest_coordinates.longitude})
       console.log(response.data)
+      const hours = response.data.analysis.data.hours
+      setMostFrequentHour(Object.keys(hours).reduce((a, b) => hours[a] > hours[b] ? a : b))
+      console.log(response.data.analysis.q1_q3.q1_q3_str_clean)
+      setq1q3(response.data.analysis.q1_q3.q1_q3)
+      setq1q3StrClean(response.data.analysis.q1_q3.q1_q3_str_clean)
+
     })
   }
   useEffect(getCitations, [])
-  const iconOptions = {
-    url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png', // Replace with your marker image URL or icon path
-    // size: 0.5, // Adjust the size as per your requirement
-  };
+  
+  // add search bar
+  // onClick make web request to API
+  // reset data
 
 
 
   return (
     <div>
       <h1>Welcome to React!</h1>
-      <div className="App">
+      <div className="App">      
+        <input type="text" value={address} onChange={(event) => {setAddress(event.target.value)}} />
+        <button onClick={getCitations}>Find Citations</button>
+
+        <div>
+          <h3>
+            Your parking tips:
+          </h3>
+          <p>The most frequent hour of tickets is {mostFrequentHour}</p>
+          <p>50% of tickets in this area are between the hours of {q1q3[0]} and {q1q3[1]}</p>
+          <p>50% of street sweeping tickets in this area are between the hours of {q1q3StrClean[0]} and {q1q3StrClean[1]}</p>
+        </div>
+        
       {!isLoaded ? (
         <h1>Loading...</h1>
       ) : (
+        
         <GoogleMap
           mapContainerClassName="map-container"
           center={center}
@@ -50,11 +76,29 @@ export function Content() {
                 position={{ lat: citation.latitude, lng: citation.longitude
                 }}
                 icon={citation.violation_desc === 'STR CLEAN' ? 'http://maps.google.com/mapfiles/ms/icons/green-dot.png' : 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'}
-              />            
-
+                onClick={() => setSelectedCitation(citation)}                                  
+              />                            
+              
           ))}
+          
+          {selectedCitation && (
+            <InfoWindowF
+              position={{ lat: selectedCitation.latitude, lng: selectedCitation.longitude
+              }}
+              onCloseClick={() => setSelectedCitation(null)}
+            >
+              <div>
+                <div>{selectedCitation.citation_issued_datetime}</div>
+                <div>{selectedCitation.violation_desc}</div>
+                <div>{selectedCitation.citation_location}</div>
+              </div>
+            </InfoWindowF>
+          )}
+
         </GoogleMap>
       )}
+      
+     
     </div>
 
     </div>
