763b6b073dde21f3b2eac8a98f82f37fd75f6676
brian <riceb53@gmail.com>
2023-06-11 16:35:11 -0700

    add aws logo

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,12 +3,13 @@ import googleMapsLogo from './assets/images/google_maps.jpeg';
 import sqlAlchemyLogo from './assets/images/sqlalchemy.png';
 import fastapiLogo from './assets/images/fastapi.jpeg';
 import openDataSFLogo from './assets/images/opendatasf.jpeg';
+import awsLogo from './assets/images/aws.png';
 
 
 export function Footer() {
   return (
     <footer>
-      <h2>Technologies Used:</h2>
+      <h2>Built with:</h2>
       <div className="technologies">
         <div className="technology">          
           <img src={reactLogo} alt="React" />;
@@ -29,6 +30,10 @@ export function Footer() {
         <div className="technology">
           <img src={sqlAlchemyLogo} alt="React" />;
           <p>SQLAlchemy</p>
+        </div>
+        <div className="technology">
+          <img src={awsLogo} alt="React" />;
+          <p>AWS</p>
         </div>
       </div>
     </footer>
